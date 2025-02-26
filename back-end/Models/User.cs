f974aa83af6c63a8e87bee350f9a47d681e5c19d
Cyprien Touraine <cyprien.touraine@gmail.com>
2025-02-26 20:34:48 +0100

    [ADD] Add model user

--- back-end/Models/User.cs
+++ back-end/Models/User.cs
@@ -1,7 +1,34 @@
+using System.Net.Mail;
+
 namespace Back_end.Models
 {
     public class User
     {
-        // TODO
+        public Guid Id { get; set; }
+        public required string Mail { get; set; }
+        public required string Pseudo { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+
+        /**
+         * Vérifie si l'email est valide
+         */
+        public static bool IsMailValid(string mail)
+        {
+            // Vérifie si l'email est vide ou null
+            if (string.IsNullOrWhiteSpace(mail))
+                return false;
+
+            // Vérifie si l'email est valide
+            try
+            {
+                var mailAddress = new MailAddress(mail);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
     }
 }
