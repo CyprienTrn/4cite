982e35ada7c71afa995c09943bb9b1462161c141
JulienBUTI <julienguilleminot45@gmail.com>
2025-02-27 16:06:55 +0100

    [TEST] Create method TestCreateBooking

--- back-end.Tests/Models/BookingTests.cs
+++ back-end.Tests/Models/BookingTests.cs
@@ -5,6 +5,24 @@ namespace back_end.Tests.Models
 {
     public class BookingTests
     {
-        
+        /**
+        * Test de la création d'une réservation avec des valeurs valides
+        */
+        [Fact]
+        public void TestCreateBooking()
+        {
+            Booking booking = new()
+            {
+                Date = new DateTime(2025, 2, 27),
+                CustomerEmail = "customer@example.com",
+                Breakfast = true,
+                RoomType = RoomTypeEnum.Deluxe
+            };
+
+            Assert.Equal(new DateTime(2025, 2, 27), booking.Date);
+            Assert.Equal("customer@example.com", booking.CustomerEmail);
+            Assert.True(booking.Breakfast);
+            Assert.Equal(RoomTypeEnum.Deluxe, booking.RoomType);
+        }
     }
 }
