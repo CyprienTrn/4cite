5bd0bc88fd7d864b2ef273c5f57e48b9bda91330
Cyprien Touraine <cyprien.touraine@gmail.com>
2025-03-01 14:27:33 +0100

    [ADD] Generate the UserController file and setup the UserController and UserControllerTest files

--- back-end.Tests/Controllers/UserControllerTests.cs
+++ back-end.Tests/Controllers/UserControllerTests.cs
@@ -1,11 +1,17 @@
+using back_end.Controllers;
+using back_end.Services;
+using Moq;
+
 namespace back_end.Tests.Controllers
 {
     public class UserControllerTests
     {
+        private readonly UserController _controller;
+        private readonly Mock<UserService> _mockService = new Mock<UserService>();
 
         public UserControllerTests()
         {
-            //TODO
+            _controller = new UserController(_mockService.Object);
         }
     }
 }
---
+++ back-end/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using back_end.Services;
+
+namespace back_end.Controllers
+{
+    public class UserController
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+    }
+}
